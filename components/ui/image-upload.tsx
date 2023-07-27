e8ebdcba9aa91772f7808cf1e4f0a6044a829d6d
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-07-27 22:58:19 +0700

    added billboards feature and setup for categories feature

---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "./button";
+import { ImagePlusIcon, TrashIcon } from "lucide-react";
+import Image from "next/image";
+import { CldUploadWidget } from 'next-cloudinary';
+
+interface ImageUploadProps {
+    disabled?: boolean;
+    onChange: (value: string) => void;
+    onRemove: (value: string) => void;
+    value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+    disabled,
+    onChange,
+    onRemove,
+    value
+}) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
+    if (!isMounted) {
+        return null
+    }
+
+    const onUpload = (result: any) => {
+        onChange(result.info.secure_url);
+    }
+
+    return (
+        <div>
+            <div className="mb-4 flex items-center gap-4">
+                {value.map((url) => (
+                    <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
+                        <div className="z-10 absolute top-2 right-2">
+                            <Button size="icon" variant={'destructive'} type="button" onClick={() => onRemove(url)}>
+                                <TrashIcon className="w-4 h-4" />
+                            </Button>
+                        </div>
+                        <Image
+                            fill
+                            className="object-cover"
+                            alt="image"
+                            src={url}
+                        />
+                    </div>
+                ))}
+            </div>
+            <CldUploadWidget
+                onUpload={onUpload}
+                uploadPreset="ehs5g95v"
+            >
+                {({ open }) => {
+                    const onClick = () => {
+                        open();
+                    }
+                    return (
+                        <Button
+                            type="button"
+                            disabled={disabled}
+                            variant={'secondary'}
+                            onClick={onClick}
+
+                        >
+                            <ImagePlusIcon className="h-4 w-4 mr-2" />
+                            Upload an Image
+                        </Button>
+                    )
+                }}
+            </CldUploadWidget>
+        </div>
+    )
+}
+
+export default ImageUpload;
